1b2ef9d7bb6ff159b55e0a4c5f449c240923415f
Rob Moran <rob.moran@arm.com>
2018-11-28 14:19:21 +0000

    Update cpp debug to use latest debug in Theia
    Signed-off-by: Rob Moran <rob.moran@arm.com>

--- packages/cpp-debug/src/node/cpp-debug-contribution.ts
+++ packages/cpp-debug/src/node/cpp-debug-contribution.ts
@@ -14,119 +14,35 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-const path = require('path');
-const packageJson = require('../../package.json');
-const debugAdapterDir = packageJson['debugAdapter']['dir'];
-
 import { injectable } from 'inversify';
 import { DebugConfiguration } from '@theia/debug/lib/common/debug-common';
-import { DebugAdapterContribution, DebugAdapterExecutable } from '@theia/debug/lib/node/debug-model';
+import { AbstractVSCodeDebugAdapterContribution } from '@theia/debug/lib/node/vscode/vscode-debug-adapter-contribution';
+import { join } from 'path';
 import * as Ajv from 'ajv';
 
-export const CppDebugConfigurationSchema = require('../../data/cpp-debug-configuration-schema.json');
+const adapterName = 'gdb';
+const adapterPath = join(__dirname, '../../adapters/code-debug');
+
+// Load schema from package.json
+const packageJson = require(join(adapterPath, 'package.json'));
+const CppDebugConfigurationSchema = packageJson.contributes.debuggers.filter((e: any) => e.type === adapterName)[0].configurationAttributes;
+
 export namespace cppDebugConfigurationValidators {
-    export const Launch = new Ajv().compile(CppDebugConfigurationSchema['launch']);
-    export const Attach = new Ajv().compile(CppDebugConfigurationSchema['attach']);
+    export const Launch = new Ajv().compile(CppDebugConfigurationSchema.launch);
+    export const Attach = new Ajv().compile(CppDebugConfigurationSchema.attach);
 }
 
 @injectable()
-export class GdbDebugAdapterContribution implements DebugAdapterContribution {
+export class GdbDebugAdapterContribution extends AbstractVSCodeDebugAdapterContribution {
 
-    readonly debugType = 'gdb';
-    readonly filePatterns = [
-        '[.]c$',
-        '[.]cpp$',
-        '[.]d$',
-        '[.]objective-c$',
-        '[.]fortran$',
-        '[.]fortran-modern$',
-        '[.]fortran90$',
-        '[.]fortran_free-form$',
-        '[.]fortran_fixed-form$',
-        '[.]rust$',
-        '[.]pascal$',
-        '[.]objectpascal$',
-        '[.]ada$',
-        '[.]nim$',
-        '[.]arm$',
-        '[.]asm$',
-        '[.]vala$',
-        '[.]crystal$',
-    ];
+    constructor() {
+        super(
+            adapterName,
+            adapterPath
+        );
+    }
 
-    provideDebugConfigurations = [
-        {
-            type: 'gdb',
-            request: 'launch',
-            name: 'Launch Program',
-            target: './bin/executable',
-            cwd: '${workspaceFolder}',
-            valuesFormatting: 'parseText'
-        },
-        {
-            type: 'gdb',
-            request: 'attach',
-            name: 'Attach to PID',
-            target: '${pid}',
-            cwd: '${workspaceFolder}',
-            valuesFormatting: 'parseText'
-        },
-        {
-            type: 'gdb',
-            request: 'attach',
-            name: 'Attach to gdbserver',
-            executable: './bin/executable',
-            target: ':2345',
-            remote: true,
-            cwd: '${workspaceFolder}',
-            valuesFormatting: 'parseText'
-        },
-        {
-            type: 'gdb',
-            request: 'launch',
-            name: 'Launch Program (SSH)',
-            target: '/bin/executable',
-            cwd: '${workspaceFolder}',
-            ssh: {
-                host: '127.0.0.1',
-                cwd: '/home/remote_user/project/',
-                keyfile: '/home/my_user/.ssh/id_rsa',
-                user: 'remote_user'
-            }
-        },
-        {
-            type: 'gdb',
-            request: 'launch',
-            name: 'Launch Program (SSH + X11)',
-            target: './bin/executable',
-            cwd: '${workspaceFolder}',
-            ssh: {
-                host: '127.0.0.1',
-                cwd: '/home/remote_user/project/',
-                keyfile: 'home/my_user/.ssh/id_rsa',
-                user: 'remote_user',
-                forwardX11: true,
-                x11host: 'localhost',
-                x11port: 6000
-            }
-        },
-        {
-            type: 'gdb',
-            request: 'launch',
-            name: 'Debug Microcontroller',
-            target: 'extended-remote /dev/cu.usbmodem00000000',
-            executable: './bin/executable.elf',
-            cwd: '${workspaceFolder}',
-            autorun: [
-                'monitor tpwr enable',
-                'monitor swdp_scan',
-                'attach 1',
-                'load ./bin/executable.elf'
-            ]
-        }
-    ];
-
-    resolveDebugConfiguration(config: DebugConfiguration): DebugConfiguration {
+    async resolveDebugConfiguration(config: DebugConfiguration, workspaceFolderUri?: string): Promise<DebugConfiguration | undefined> {
 
         switch (config.request) {
             case 'launch': return this.validateConfiguration(cppDebugConfigurationValidators.Launch, config);
@@ -141,13 +57,5 @@ export class GdbDebugAdapterContribution implements DebugAdapterContribution {
             throw new Error(validator.errors!.map(e => e.message).join(' // '));
         }
         return config;
-    }
-
-    provideDebugAdapterExecutable(config: DebugConfiguration): DebugAdapterExecutable {
-        const program = path.join(__dirname, `../../../${debugAdapterDir}/out/src/gdb.js`);
-        return {
-            program,
-            runtime: 'node',
-        };
     }
 }
